2024e71670b5686ce427b0c615701c8f26ac30ba
ambefrankline <www.ambefrank@gmail.com>
2023-07-24 13:21:09 -0400

    new projects

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # ambe-application-codes
 use for storing application codes
+
+
+
+# For the registration form project
+
+# Serverless Registration Form
+---------------------------------
+
+## Step 1: Create DynamoDB Table
+
+```sh
+Table Name: registration-table
+Partition key: email
+
+```
+
+## Step 2: Create IAM Role for Lambda Function
+```sh
+IAM Role Name: RegistrationFormRole
+
+Permissions:
+1. CloudWatch Full Access
+2. DynamoDB Full Access
+
+```
+
+## Step 3: Create Lambda Function
+
+```sh
+Function Name: registration-form-function
+Runtime: Python 3.9
+
+```
+
+## Step 4: Write Lambda Function
+
+## Step 5: Create API Gateway and Enable CORS
+
+## Step 6: Create an S3 Bucket and  Test the Project
+
+
+### Enable CORS: 
+
+```sh
+Access-Control-Allow-Origin: '*'
+Access-Control-Allow-Headers: Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token
+Access-Control-Allow-Methods: POST
+
+```
+
+## Step 5: Test the Application
+
+
+
+Follow our tutorials here: https://www.youtube.com/@amonkincloud/videos \
+Follow our blog here: https://amonkincloud.com/ \
+Follow my personal blog here:https://dev.to/yeshwanthlm/ \
+Follow us on Instagram: https://www.instagram.com/amonkincloud/ \
+For queries write to us at: amonkincloud@gmail.com 
+
+## for the amazone connect project
+ ## Created a customer service call center with amazon connect for a ccompany called: ambe Gadget Company
+ --------------------------------------------------
+youtube tutorial:
+ https://www.youtube.com/watch?v=yKNb6db_d7c&list=PLGhcieJ42HtH7fksyhSnTu4mv6G1HsU9o&ab_channel=ArchitectureBytes
---
+++ facial/src/App.js
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import './App.css';
+const uuid = require('uuid');
+
+function App() {
+  const [image, setImage] = useState('');
+  const [uploadResultMessage, setUploadResultMessage] = useState('Pleaase Upload An Image To Authenticate.')
+  const [visitorName, setVisitorName] = useState('placeholder.jpg');
+  const [isAuth, setAuth] = useState(false);
+
+
+  function sendImage(e) {
+    e.preventDefault();
+    setVisitorName(image.name);
+    const visitorImageName = uuid.v4();
+    fetch(`https://mmnxg16lgl.execute-api.us-east-1.amazonaws.com/dev/frank-visitors-image-storage/${visitorImageName}.jpeg`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'image/jpeg'
+      },
+      body: image
+    }).then(async () => {
+      const response = await authenticate(visitorImageName);
+      if (response.Message === 'Success') {
+        setAuth(true);
+        setUploadResultMessage(`Hi ${response['firstName']} ${response['lastName']}, Welcome to work. Hope you have a productive day Today!!`)
+      } else {
+        setAuth(false);
+        setUploadResultMessage('Authenticate Failed: this person is not an employee.')
+      }
+    }).catch(error => {
+      setAuth(false);
+      setUploadResultMessage('There is an error during the authentication process. Please try again.')
+      console.error(error);
+    })
+  }
+
+  async function authenticate(visitorImageName) {
+    const requestUrl = 'https://mmnxg16lgl.execute-api.us-east-1.amazonaws.com/dev/employee?' + new URLSearchParams({
+      objectKey: `${visitorImageName}.jpeg`
+    });
+    return await fetch(requestUrl, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then(response => response.json())
+      .then((data) => {
+        return data;
+      }).catch(error => console.error(error));
+  }
+
+  return (
+    <div className="App">
+      <h2>Frank's Facial Recognition System</h2>
+      <form onSubmit={sendImage}>
+        <input type='file' name='image' onChange={e => setImage(e.target.files[0])} />
+        <button type='submit'>Authenticate</button>
+      </form>
+      <div className={isAuth ? 'success' : 'failure'}>{uploadResultMessage}</div>
+      <img src={require(`./visitors/${visitorName}`)} alt="visitor" height={250} width={250} />
+    </div>
+  );
+}
+
+export default App;
